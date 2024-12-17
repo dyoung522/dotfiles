6b39b42b211ff3a894a759b76287e46ce31905be
Donovan Young <dyoung522@gmail.com>
2024-12-17 12:13:47 -0500

    Update .oh-my-zsh/custom/ruby.zsh

--- dot_oh-my-zsh/custom/ruby.zsh
+++ dot_oh-my-zsh/custom/ruby.zsh
@@ -1,2 +1,3 @@
-which -s rbenv > /dev/null 2>&1 && eval "$(rbenv init - --no-rehash zsh)"
+#which -s rbenv > /dev/null 2>&1 && eval "$(rbenv init - --no-rehash zsh)"
+evalIfWhich rbenv rbenv init - --no-rehash zsh
 
